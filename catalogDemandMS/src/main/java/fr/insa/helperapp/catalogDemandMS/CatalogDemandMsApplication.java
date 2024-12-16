a7669161533dde93bf67def8272a8609c33e0e4f
Hugo.M <151736047+MICHEL-Hugo@users.noreply.github.com>
2024-12-16 16:47:32 +0100

    Add CatalogDemandMS Structure

---
+++ catalogDemandMS/src/main/java/fr/insa/helperapp/catalogDemandMS/CatalogDemandMsApplication.java
@@ -0,0 +1,13 @@
+package fr.insa.helperapp.catalogDemandMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogDemandMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogDemandMsApplication.class, args);
+	}
+
+}
