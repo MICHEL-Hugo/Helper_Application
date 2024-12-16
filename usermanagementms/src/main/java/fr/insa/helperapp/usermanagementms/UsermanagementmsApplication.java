b550291f0c81347ce32266ba6a364b22cb2c5fdf
amasson <masson.alex42240@gmail.com>
2024-12-16 17:20:00 +0100

    modifié :         config/src/main/resources/application.properties 	nouveau fichier : discovery.properties 	nouveau fichier : usermanagementms.properties 	nouveau fichier : usermanagementms/.gitattributes 	nouveau fichier : usermanagementms/.gitignore 	nouveau fichier : usermanagementms/.mvn/wrapper/maven-wrapper.properties 	nouveau fichier : usermanagementms/mvnw 	nouveau fichier : usermanagementms/mvnw.cmd 	nouveau fichier : usermanagementms/pom.xml 	nouveau fichier : usermanagementms/src/main/java/fr/insa/helperapp/usermanagementms/UsermanagementmsApplication.java 	nouveau fichier : usermanagementms/src/main/resources/application.properties 	nouveau fichier : usermanagementms/src/test/java/fr/insa/helperapp/usermanagementms/UsermanagementmsApplicationTests.java

---
+++ usermanagementms/src/main/java/fr/insa/helperapp/usermanagementms/UsermanagementmsApplication.java
@@ -0,0 +1,13 @@
+package fr.insa.helperapp.usermanagementms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermanagementmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermanagementmsApplication.class, args);
+	}
+
+}
---
+++ usermanagementms/src/test/java/fr/insa/helperapp/usermanagementms/UsermanagementmsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insa.helperapp.usermanagementms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermanagementmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
