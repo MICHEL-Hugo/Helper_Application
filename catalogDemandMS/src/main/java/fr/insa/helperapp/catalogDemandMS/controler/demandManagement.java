c036e543ebf564aa3deccbc211571107ab87f7c2
Hugo.M <151736047+MICHEL-Hugo@users.noreply.github.com>
2024-12-22 22:33:21 +0100

    Update demandManagement.java

--- catalogDemandMS/src/main/java/fr/insa/helperapp/catalogDemandMS/controler/demandManagement.java
+++ catalogDemandMS/src/main/java/fr/insa/helperapp/catalogDemandMS/controler/demandManagement.java
@@ -9,7 +9,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/requests")
+@RequestMapping("/demands")
 public class demandManagement {
 
     @Autowired
